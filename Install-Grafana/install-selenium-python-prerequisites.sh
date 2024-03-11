ebeb6af5bbb211cc13f58afea07655217f2e3448
Veronika Klauzova <git@klauzova.com>
2024-03-11 08:51:28 +0100

    Update install-selenium-python-prerequisites.sh

--- Install-Grafana/install-selenium-python-prerequisites.sh
+++ Install-Grafana/install-selenium-python-prerequisites.sh
@@ -4,9 +4,14 @@
 # Description: Installing pre-requisites
 ###############################
 
+sudo apt-get install aptitude -y
+sudo aptitude update
+# upgrade chromium as we need at minim 122v 
+sudo aptitude upgrade chromium-browser -y
+
 sudo aptitude install chromium-chromedriver -y
 mkdir ~/.local
 python3 -m venv ~/.local
 ~/.local/bin/pip3 install requests
-~/.local/bin/pip3 install pytest
+#~/.local/bin/pip3 install pytest
 ~/.local/bin/pip3 install selenium 
